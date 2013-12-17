7abd1b1ea8a9a2629b3ca0dbca0f553dad9959f1
Vladislav <vlad@holey.org>
2013-12-17 23:30:36 +0300

    Adding demo level

--- Assets/Script/Components/Terrain.cs
+++ Assets/Script/Components/Terrain.cs
@@ -6,12 +6,15 @@ namespace Assets.Script.Components
 {
     public class Terrain : MonoBehaviour
     {
+        public bool DisplayFieldGizmo = false;
+        public bool DisplayPathGizmo = false;
+
         public void Start()
         {
             PathFinderGlobal.Terrain = gameObject;
             updateGrid();
 
-            PathFinderGlobal.Find(new WaveFinder(), new Vector3(-5, 0, -5), new Vector3(4, 0, 4));
+            PathFinderGlobal.Find(new WaveFinder(), new Vector3(-9, 0, -9), new Vector3(9, 0, 9));
         }
 
         public void Update()
@@ -25,44 +28,49 @@ namespace Assets.Script.Components
                 return;
             }
 
-            var startX = PathFinderGlobal.TerrainFieldStartX;
-            var startZ = PathFinderGlobal.TerrainFieldStartZ;
             var correction = PathFinderGlobal.CellCorrection;
 
-            var iMax = PathFinderGlobal.TerrainFieldWidth;
-            var jMax = PathFinderGlobal.TerrainFieldHeight;
-
-            var cubeGizmoSize = new Vector3(PathFinderGlobal.CellWidth, 1, PathFinderGlobal.CellWidth) - new Vector3(0.1f, 0f, 0.1f);
-
-            for (var i = 0; i < iMax; i++)
+            if (DisplayFieldGizmo)
             {
-                var x = startX + PathFinderGlobal.CellWidth * i + correction;
-                for (var j = 0; j < jMax; j++)
+                var startX = PathFinderGlobal.TerrainFieldStartX;
+                var startZ = PathFinderGlobal.TerrainFieldStartZ;
+                
+                var iMax = PathFinderGlobal.TerrainFieldWidth;
+                var jMax = PathFinderGlobal.TerrainFieldHeight;
+
+                var cubeGizmoSize = new Vector3(PathFinderGlobal.CellWidth, 1, PathFinderGlobal.CellWidth) - new Vector3(0.1f, 0f, 0.1f);
+
+                for (var i = 0; i < iMax; i++)
                 {
-                    Gizmos.color = PathFinderGlobal.TerrainField[i, j] != null && PathFinderGlobal.TerrainField[i, j].Blocked
-                        ? Color.red
-                        : Color.green;
-                    
-                    var z = startZ + PathFinderGlobal.CellWidth * j + correction;
-                    var startPosition = new Vector3(x, 0.5f, z) + new Vector3(0.1f, 0f, 0.1f);
-                    Gizmos.DrawWireCube(startPosition, cubeGizmoSize);
+                    var x = startX + PathFinderGlobal.CellWidth * i + correction;
+                    for (var j = 0; j < jMax; j++)
+                    {
+                        Gizmos.color = PathFinderGlobal.TerrainField[i, j] != null && PathFinderGlobal.TerrainField[i, j].Blocked
+                            ? Color.red
+                            : Color.green;
+
+                        var z = startZ + PathFinderGlobal.CellWidth * j + correction;
+                        var startPosition = new Vector3(x, 0.5f, z) + new Vector3(0.1f, 0f, 0.1f);
+                        Gizmos.DrawWireCube(startPosition, cubeGizmoSize);
+                    }
                 }
             }
 
-            /* Temp */
-            if (PathFinderGlobal.LastResult != null && PathFinderGlobal.LastResult.Any())
+            if (DisplayPathGizmo)
             {
-                Gizmos.color = Color.blue;
-                Gizmos.DrawWireSphere(PathFinderGlobal.LastResult.First() + new Vector3(correction, 0, correction), .2f);
-                Gizmos.DrawWireSphere(PathFinderGlobal.LastResult.Last() + new Vector3(correction, 0, correction), .2f);
-
-                Gizmos.color = Color.yellow;
-                foreach (var point in PathFinderGlobal.LastResult)
+                if (PathFinderGlobal.LastResult != null && PathFinderGlobal.LastResult.Any())
                 {
-                    Gizmos.DrawWireSphere(point + new Vector3(correction, 0, correction), .1f);
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawWireSphere(PathFinderGlobal.LastResult.First() + new Vector3(correction, 0, correction), .2f);
+                    Gizmos.DrawWireSphere(PathFinderGlobal.LastResult.Last() + new Vector3(correction, 0, correction), .2f);
+
+                    Gizmos.color = Color.red;
+                    foreach (var point in PathFinderGlobal.LastResult)
+                    {
+                        Gizmos.DrawWireSphere(point + new Vector3(correction, 0, correction), .1f);
+                    }
                 }
             }
-            /* Temp */
         }
 
         private void updateGrid()
---
+++ Assets/Script/Demo/FinderCamera.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Script.Finder;
+using UnityEngine;
+
+namespace Assets.Script.Demo
+{
+    public class FinderCamera : MonoBehaviour
+    {
+        private Vector3 _startPoint = new Vector3(0, 0, 0);
+        private Vector3 _endPoint = new Vector3(0, 0, 0);
+
+        public void Start()
+        {
+        }
+	
+        public void Update()
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Input.GetMouseButtonUp(0) && Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 31))
+            {
+                _endPoint = hit.point;
+                PathFinderGlobal.Find(new WaveFinder(), _startPoint, _endPoint);
+                _startPoint = _endPoint;
+            }
+        }
+    }
+}

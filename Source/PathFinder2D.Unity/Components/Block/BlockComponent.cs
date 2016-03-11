be2e0a0b6647d270b81ee8796faecde2de304fa6
Vladislav <vladislavnekhaichik@gmail.com>
2016-03-12 00:58:26 +0300

    cleanup

--- Source/PathFinder2D.Unity/Components/Block/BlockComponent.cs
+++ Source/PathFinder2D.Unity/Components/Block/BlockComponent.cs
@@ -8,8 +8,6 @@ namespace PathFinder2D.Unity.Components.Block
     [AddComponentMenu("Modules/PathFinder2D/Blocks/Block")]
     public class BlockComponent : MonoBehaviour, IBlock
     {
-        public bool UpdatePosition;
-
         public IEnumerable<WorldPosition> GetPoints(IFloor floor)
         {
             return new[] { new WorldPosition(transform.position.x, transform.position.z) };
